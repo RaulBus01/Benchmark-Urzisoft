e4841303c17fb8f09fb8ca1be222f97a3a353391
Virgil <virgil.crisan@student.upt.ro>
2023-05-26 02:57:30 +0300

    added Launcher to wrap the whole app

---
+++ src/main/java/com/example/Launcher.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import com.example.frontend_urzisoft.HelloApplication;
+
+public class Launcher {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
